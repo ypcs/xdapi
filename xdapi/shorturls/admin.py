0e0cfffa9c588b4742c99ac3905987eafb8eeaa0
Ville Korhonen <ville@xd.fi>
2013-08-03 14:09:06 +0300

    Added TODO

--- xdapi/shorturls/admin.py
+++ xdapi/shorturls/admin.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from shorturls.models import ShortURL
 
+# TODO: Allow searching by URL, for example "I wan't to see what keys link to this URL"
+
 class ShortURLAdmin(admin.ModelAdmin):
     list_display = ('key', 'url', 'title', 'owner', 'visit_count', 'status',)
 
