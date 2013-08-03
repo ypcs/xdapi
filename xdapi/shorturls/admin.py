820d127c868e6fa7c3415474f884c8281260fe01
Ville Korhonen <ville@xd.fi>
2013-08-03 13:06:01 +0300

    Added status to admin view

--- xdapi/shorturls/admin.py
+++ xdapi/shorturls/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from shorturls.models import ShortURL
 
 class ShortURLAdmin(admin.ModelAdmin):
-    list_display = ('key', 'url', 'title', 'visit_count',)
+    list_display = ('key', 'url', 'title', 'visit_count', 'status',)
 
 admin.site.register(ShortURL, ShortURLAdmin)
