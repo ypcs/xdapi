083a6ceddf6362333c76b6fd4492372c27936d9f
Ville Korhonen <ville@xd.fi>
2013-08-03 13:45:16 +0300

    Get page title, if not defined

--- xdapi/shorturls/models.py
+++ xdapi/shorturls/models.py
@@ -4,6 +4,9 @@ from django.core.validators import RegexValidator
 from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
 from django.conf import settings
 
+from bs4 import BeautifulSoup
+import requests
+
 _ = lambda x:x
 
 SHORTURL_KEY_REGEX = r'^[\w\d:-]{3,255}'
@@ -16,6 +19,8 @@ class ShortURL(models.Model):
         ('V', _('Verified')),   # Manually checked & verified, OK
         ('E', _('Expired')),    # Expired, do not redirect
         ('C', _('Confirm')),    # Confirm, that user really want's to continue
+        ('w', _('WebShot TODO')),    # Generate webshot from this
+        ('W', _('WebShot Generated')), # WebShot for this exists
     )
     SHORTURL_DISABLED_STATUSES = ('R', 'S', 'E',)
 
@@ -43,8 +48,22 @@ class ShortURL(models.Model):
         self.visit_count += 1
         self.save()
 
+    def get_page_title(self):
+        req = requests.get(self.url)
+        if req.status_code == 200:
+            soup = BeautifulSoup(req.text)
+            return soup.title.string
+        else:
+            return ""
+
     def __str__(self):
         return "%s => %s" % (self.key, self.url)
+
+    def save(self, *args, **kwargs):
+        print self.title
+        if self.title == "":
+            self.title = self.get_page_title()
+        super(ShortURL, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Short URL'
