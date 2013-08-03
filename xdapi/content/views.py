0d09e51349b4eeb06dc6927ac5821bc17f2252b8
Ville Korhonen <ville@xd.fi>
2013-08-03 16:35:09 +0300

    Added TODO

--- xdapi/content/views.py
+++ xdapi/content/views.py
@@ -6,6 +6,7 @@ from django.views.generic.base import TemplateView, RedirectView
 
 # TODO: If shorturl, and autoredirect & redirect allowed => return redirect url
 # TODO: If other content, or autoredirect disabled, or redirect not allowed => display preview
+# TODO: Fileuploads: create CF urls w/ access token, locked to specific IP, and set access time to 10min etc., ie. force using our site to access
 
 class ContentView(TemplateView):
     pass
